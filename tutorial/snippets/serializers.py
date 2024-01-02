550b2493a74e689f0ee1f29c18819d3d957b429e
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-01-02 22:53:57 +0900

    tutorial 4

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
-
+from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner',]
 
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
 # 포스트맨으로 계정에접속
 # admin@naver.com / dorldirkwk10!
-        
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,15 +6,42 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import mixins
 from rest_framework import generics
+from snippets.serializers import UserSerializer
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+
 # Create your views here.
 class SnippetList(generics.ListCreateAPIView):
+    
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,
+                          ]
+    
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+from django.contrib.auth.models import User
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     
 # class SnippetList(mixins.ListModelMixin,
 #                     mixins.CreateModelMixin,
