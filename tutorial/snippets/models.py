069327899c0b4f7f2478f91d0e2b2dfee44a28c3
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-01-09 22:04:01 +0900

    git add

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -12,7 +12,7 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
-
+#from django.contrib.auth.models import User
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
@@ -21,7 +21,7 @@ class Snippet(models.Model):
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
     owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
-    highlist = models.TextField()
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ['created']
